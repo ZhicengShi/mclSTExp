2d00256b534c74c4c80f7b71b68fa93965c1c7cc
shizhiceng <65576307+shizhiceng@users.noreply.github.com>
2024-04-08 20:11:29 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -22,3 +22,58 @@ def get_lr(optimizer):
 
 
 
+import numpy as np
+from scipy.stats import pearsonr
+
+
+class AvgMeter:
+    def __init__(self, name="Metric"):
+        self.name = name
+        self.reset()
+
+    def reset(self):
+        self.avg, self.sum, self.count = [0] * 3
+
+    def update(self, val, count=1):
+        self.count += count
+        self.sum += val * count
+        self.avg = self.sum / self.count
+
+    def __repr__(self):
+        text = f"{self.name}: {self.avg:.4f}"
+        return text
+
+
+def get_lr(optimizer):
+    for param_group in optimizer.param_groups:
+        return param_group["lr"]
+
+
+def get_R(data1, data2, dim=1, func=pearsonr):
+    adata1 = data1.X
+    adata2 = data2.X
+    r1, p1 = [], []
+    for g in range(data1.shape[dim]):
+        if dim == 1:
+            r, pv = func(adata1[:, g], adata2[:, g])
+        elif dim == 0:
+            r, pv = func(adata1[g, :], adata2[g, :])
+        r1.append(r)
+        p1.append(pv)
+    r1 = np.array(r1)
+    p1 = np.array(p1)
+    return r1, p1
+    
+def cluster(adata, label):
+    idx = label != 'undetermined'
+    tmp = adata[idx]
+    l = label[idx]
+    sc.pp.pca(tmp, n_comps=9)
+    sc.tl.tsne(tmp)
+    kmeans = KMeans(n_clusters=len(set(l)), init="k-means++", random_state=0).fit(tmp.obsm['X_pca'])
+    p = kmeans.labels_.astype(str)
+    lbl = np.full(len(adata), str(len(set(l))))
+    lbl[idx] = p
+    adata.obs['kmeans'] = lbl
+    nmi = normalized_mutual_info_score(l, p)
+    return p, round(ari_score(p, l), 3), round(nmi, 3)
